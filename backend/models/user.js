bd48a622813e62d380feb1964a02d9587a181f70
Kareem Ehab <kareem8002010@gmail.com>
2021-04-25 00:25:44 +0200

    Frontend Version 2.0

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,64 +1,71 @@
-const mongoose = require('mongoose');
-const Joi = require('joi');
-const passwordComplexity = require('joi-password-complexity');
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const userSchema = new mongoose.Schema({
-    nationalID: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        minlength: 6,
-        maxlength: 200,
-        default: '^&^$%^#^%^*&^*&@&^#@^%#*&^$%^&#'
-    },
-    isRegistered: {
-        type: Boolean,
-        default: false
-    },
-    email: {
-        type: String,
-        minlength: 6,
-        maxlength: 200,
-        default:'!@#$%'
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    }
+  nationalID: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    minlength: 6,
+    maxlength: 200,
+    default: "^&^$%^#^%^*&^*&@&^#@^%#*&^$%^&#",
+  },
+  isRegistered: {
+    type: Boolean,
+    default: false,
+  },
+  email: {
+    type: String,
+    minlength: 6,
+    maxlength: 200,
+    default: "!@#$%",
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  hasVoted: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
-    return token;
-}
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config.get("jwtPrivateKey")
+  );
+  return token;
+};
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
-    const schema = Joi.object({
-        nationalID: Joi.string().required().length(14),
-        isRegistered: Joi.bool(),
-        email: Joi.string().min(6).max(200).email()
-    });
-    return schema.validate(user);
+  const schema = Joi.object({
+    nationalID: Joi.string().required().length(14),
+    isRegistered: Joi.bool(),
+    email: Joi.string().min(6).max(200).email(),
+  });
+  return schema.validate(user);
 }
 
 function validatePassword(password) {
-    const complexityOptions = {
-        min: 6,
-        max: 30,
-        lowerCase: 1,
-        upperCase: 0,
-        numeric: 0,
-        symbol: 0,
-        requirementCount: 2,
-      };
-      return passwordComplexity(complexityOptions).validate(password);
+  const complexityOptions = {
+    min: 6,
+    max: 30,
+    lowerCase: 1,
+    upperCase: 0,
+    numeric: 0,
+    symbol: 0,
+    requirementCount: 2,
+  };
+  return passwordComplexity(complexityOptions).validate(password);
 }
 
 exports.User = User;
--- src/components/Candidates.js
+++ src/components/Candidates.js
@@ -1,14 +1,211 @@
-import React from 'react'
+import React from "react";
+import "../static/candidate_styles.css";
 
-const Candidates = ({candidates, token}) => {
-    console.log(token);
-    return (
-        <div>
-            {candidates.length > 0 ? (
-                candidates.map((candidate) => (<h1>{candidate.name}</h1>))
-            ) : ("There are currently no candidates")}
+const Candidates = ({ candidates, token }) => {
+  console.log(token);
+  return (
+    // <div>
+    //     {candidates.length > 0 ? (
+    //         candidates.map((candidate) => (<h1>{candidate.name}</h1>))
+    //     ) : ("There are currently no candidates")}
+    // </div>
+    <div>
+      <head>
+        <link
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="stylesheet"
+        />
+        <link rel="stylesheet" type="text/css" href="../" />
+      </head>
+
+      <body>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-12 col-sm-8 col-lg-6">
+              {/* Section Heading */}
+              <div
+                class="section_heading text-center wow fadeInUp"
+                data-wow-delay="0.2s"
+                style={{
+                  visibility: "visible",
+                  animationDelay: 0.2,
+                  animationName: "fadeInUp",
+                }}
+              >
+                <h3>Candidates</h3>
+                <p>
+                  Please cast your vote for one of the available candidates.
+                </p>
+                <div class="line"></div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            {candidates.length > 0
+              ? candidates.map((candidate) => (
+                  // Candidate
+                  <div class="col-12 col-sm-6 col-lg-3">
+                    <div
+                      class="single_advisor_profile wow fadeInUp"
+                      data-wow-delay="0.2s"
+                      style={{
+                        visibility: "visible",
+                        animationDelay: 0.2,
+                        animationName: "fadeInUp",
+                      }}
+                    >
+                      {/* Candidate Avatar */}
+                      <div class="advisor_thumb">
+                        <img
+                          src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                          alt=""
+                        />
+                      </div>
+                      {/* Candidate Details */}
+                      <div class="single_advisor_details_info">
+                        <h6>{candidate.name}</h6>
+                        <p class="designation">Candidate symbol</p>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              : "There are currently no candidates"}
+
+            {/* Candidate */}
+            <div class="col-12 col-sm-6 col-lg-3">
+              <div
+                class="single_advisor_profile wow fadeInUp"
+                data-wow-delay="0.2s"
+                style={{
+                  visibility: "visible",
+                  animationDelay: 0.2,
+                  animationName: "fadeInUp",
+                }}
+              >
+                {/* Candidate Avatar */}
+                <div class="advisor_thumb">
+                  <img
+                    src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                    alt=""
+                  />
+                </div>
+                {/* Candidate Details */}
+                <div class="single_advisor_details_info">
+                  <h6>Simon Jackson</h6>
+                  <p class="designation">Candidate symbol</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Candidate */}
+            <div class="col-12 col-sm-6 col-lg-3">
+              <div
+                class="single_advisor_profile wow fadeInUp"
+                data-wow-delay="0.2s"
+                style={{
+                  visibility: "visible",
+                  animationDelay: 0.2,
+                  animationName: "fadeInUp",
+                }}
+              >
+                {/* Candidate Avatar */}
+                <div class="advisor_thumb">
+                  <img
+                    src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                    alt=""
+                  />
+                </div>
+                {/* Candidate Details */}
+                <div class="single_advisor_details_info">
+                  <h6>Simon Jackson</h6>
+                  <p class="designation">Candidate symbol</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Candidate */}
+            <div class="col-12 col-sm-6 col-lg-3">
+              <div
+                class="single_advisor_profile wow fadeInUp"
+                data-wow-delay="0.3s"
+                style={{
+                  visibility: "visible",
+                  animationDelay: 0.3,
+                  animationName: "fadeInUp",
+                }}
+              >
+                {/* Candidate Avatar */}
+                <div class="advisor_thumb">
+                  <img
+                    src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                    alt=""
+                  />
+                </div>
+                {/* Candidate Details */}
+                <div class="single_advisor_details_info">
+                  <h6>Eslam Hossam</h6>
+                  <p class="designation">Candidate symbol</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Candidate */}
+            <div class="col-12 col-sm-6 col-lg-3">
+              <div
+                class="single_advisor_profile wow fadeInUp"
+                data-wow-delay="0.4s"
+                style={{
+                  visibility: "visible",
+                  animationDelay: 0.3,
+                  animationName: "fadeInUp",
+                }}
+              >
+                {/* Candidate Avatar */}
+                <div class="advisor_thumb">
+                  <img
+                    src="https://bootdey.com/img/Content/avatar/avatar6.png"
+                    alt=""
+                  />
+                </div>
+                {/* Candidate Details */}
+                <div class="single_advisor_details_info">
+                  <h6>Riyad Mohamed</h6>
+                  <p class="designation">Candidate symbol</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Candidate */}
+            <div class="col-12 col-sm-6 col-lg-3">
+              <div
+                class="single_advisor_profile wow fadeInUp"
+                data-wow-delay="0.5s"
+                style={{
+                  visibility: "visible",
+                  animationDelay: 0.3,
+                  animationName: "fadeInUp",
+                }}
+              >
+                {/* Candidate Avatar */}
+                <div class="advisor_thumb">
+                  <img
+                    src="https://bootdey.com/img/Content/avatar/avatar2.png"
+                    alt=""
+                  />
+                </div>
+                {/* Candidate Details */}
+                <div class="single_advisor_details_info">
+                  <h6>Mohamed Esmat</h6>
+                  <p class="designation">Candidate symbol</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </body>
+    </div>
+  );
+};
 
-export default Candidates
+export default Candidates;
