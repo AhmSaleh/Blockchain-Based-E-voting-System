8c227da15c85bf6f281c980993dd83fcdd0b9ebb
Kareem Ehab <kareem8002010@gmail.com>
2021-05-01 22:41:37 +0200

    Some synchronization between frontend and backend

--- src/components/NewCandidate.js
+++ src/components/NewCandidate.js
@@ -4,6 +4,7 @@ import "semantic-ui-css/semantic.min.css";
 import web3 from "../web3";
 import ballot from "../ballot";
 import Layout from "./Layout";
+import swal from "sweetalert";
 
 class NewCandidate extends React.Component {
   state = {
@@ -36,20 +37,49 @@ class NewCandidate extends React.Component {
     // TODO: Insert the new Candidate info into DB
     // To access these variables just write
     // this.state.candidateName/candidateSymbol
+    const param = {
+      name: this.state.candidateName,
+      symbol: this.state.candidateSymbol,
+      photo: this.state.candidatePhoto,
+    };
+
+    axios
+      .post("http://localhost:5000/api/candidates", param, {
+        headers: {
+          "x-auth-token":
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDdiMDI5NjA5MDc5OTczYTA1OGNkZDAiLCJpc0FkbWluIjp0cnVlLCJoYXNWb3RlZCI6ZmFsc2UsImlhdCI6MTYxOTY5OTU1OX0.cKcjFm-rkAwpCB_eXjoM85o_yChY-Ew4fnaaM2MpsGs",
+        },
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          swal(
+            "Success!",
+            "Candidate added to the election successfully!",
+            "success"
+          );
+          window.location.pathname = "/admin";
+        } else if (res.status === 401)
+          swal("Error!", "Unauthenticated!", "error");
+        else if (res.status === 403) swal("Error!", "Unauthorized!", "error");
+      })
+      .catch((err) => {
+        swal("Error!", "Failed to add candidate!", "error");
+        console.log(err.message);
+      });
   };
 
   imageHandler = (e) => {
     const reader = new FileReader();
-    reader.onload = () =>{
-      if(reader.readyState === 2){
-        this.setState({candidatePhoto: reader.result})
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        this.setState({ candidatePhoto: reader.result });
       }
-    }
-    reader.readAsDataURL(e.target.files[0])
+    };
+    reader.readAsDataURL(e.target.files[0]);
   };
 
   render() {
-    const { candidatePhoto } = this.state
+    const { candidatePhoto } = this.state;
     return (
       <Layout>
         <div class="new_candidate_form">
@@ -76,11 +106,17 @@ class NewCandidate extends React.Component {
             </Form.Field>
             <Form.Field>
               <label>Photo</label>
-              <input type="file" accept="image/*" name="image-upload" id="input" onChange={this.imageHandler} />
+              <input
+                type="file"
+                accept="image/*"
+                name="image-upload"
+                id="input"
+                onChange={this.imageHandler}
+              />
             </Form.Field>
             <div className="img-holder">
-						  <Image src={candidatePhoto} alt="" size='medium' rounded/>
-					  </div>
+              <Image src={candidatePhoto} alt="" size="medium" rounded />
+            </div>
             <Message error header="Oops!" content={this.state.errorMessage} />
             <Button
               fluid
